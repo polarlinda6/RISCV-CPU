f21402e740dbae387d02be06f4dee1bbd833bddc
polarlinda6 <101777164+polarlinda6@users.noreply.github.com>
2025-04-28 17:44:42 +0800

    refactor(registers): 移除未使用的寄存器初始化代码
    清理模块中未使用的寄存器初始化代码，以简化代码结构并提高可读性

--- registers.v
+++ registers.v
@@ -26,37 +26,8 @@ module registers(
 	begin
 		if(!rst_n)
 		begin
-//			regs[1]  <= `zeroword;
 			regs[2]  <= 32'h7ffffff0;  //sp
 			regs[3]  <= 32'h10000000;  //gp
-//			regs[4]  <= `zeroword;
-//			regs[5]  <= `zeroword;
-//			regs[6]  <= `zeroword;
-//			regs[7]  <= `zeroword;
-//			regs[8]  <= `zeroword;
-//			regs[9]  <= `zeroword;
-//			regs[10] <= `zeroword;
-//			regs[11] <= `zeroword;
-//			regs[12] <= `zeroword;
-//			regs[13] <= `zeroword;
-//			regs[14] <= `zeroword;
-//			regs[15] <= `zeroword;
-//			regs[16] <= `zeroword;
-//			regs[17] <= `zeroword;
-//			regs[18] <= `zeroword;
-//			regs[19] <= `zeroword;
-//			regs[20] <= `zeroword;
-//			regs[21] <= `zeroword;
-//			regs[22] <= `zeroword;
-//			regs[23] <= `zeroword;
-//			regs[24] <= `zeroword;
-//			regs[25] <= `zeroword;
-//			regs[26] <= `zeroword;
-//			regs[27] <= `zeroword;
-//			regs[28] <= `zeroword;
-//			regs[29] <= `zeroword;
-//			regs[30] <= `zeroword;
-//			regs[31] <= `zeroword;
 		end
 		else if(RegWrite) 
 			regs[Rd] <= Wr_data;
