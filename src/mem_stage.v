9a510bb80a115300b75c8aadc74a3a751f3e2b7b
polarlinda6 <101777164+polarlinda6@users.noreply.github.com>
2025-05-14 19:37:35 +0800

    style: 调整代码格式和缩进以提高可读性
    对 `src/mem_stage.v` 文件中的代码进行了格式调整，统一了缩进和括号的格式，使其更符合代码风格规范。此外，对 `src/rom_binary_file.txt` 文件中的二进制数据进行了小幅调整，确保数据一致性。

--- src/mem_stage.v
+++ src/mem_stage.v
@@ -8,19 +8,19 @@ module mem_stage(
 	input  [31:0]load_data_mem_i,
 	input  MemRead_mem_i,
 	output [31:0]load_or_result_mem_o
-    );
+);
 
 	mux mem_mux (
         .din1(load_data_mem_wb_o), 
         .din2(Rs2_data_mem_i), 
         .signal(forward_load_mem_i), 
         .dout(store_data)
-        );
+    );
         
    	mux wb_data_mux (
         .din1(load_data_mem_i), 
         .din2(result_mem_i), 
         .signal(MemRead_mem_i), 
         .dout(load_or_result_mem_o)
-        );     
+    );     
 endmodule
