62d8ee3a5723dd33d8a3f464e24cf065eade1477
polarlinda6 <101777164+polarlinda6@users.noreply.github.com>
2025-05-15 15:34:01 +0800

    style(registers): 优化代码格式以提高可读性
    调整模块`registers.v`中的代码格式，统一缩进并简化`always`块的结构，使代码更易于阅读和维护。

--- src/registers.v
+++ src/registers.v
@@ -17,48 +17,45 @@ module registers(
 	input  [4:0]prediction_Rs2,
 	output [31:0]prediction_Rs1_data,
 	output [31:0]prediction_Rs2_data
-    );
+  );
 	
 	reg [31:0]regs[31:1]; //x0 is a dedicated zero register.
 	                      //Not implemented to save resources.
 
-	always@(negedge clk)
-	begin
-		if(!rst_n)
-		begin
-			regs[1]  <= `zeroword;
-			regs[`sp]  <= 32'h7ffffff0;
-			regs[`gp]  <= 32'h10000000;
-			regs[4]  <= `zeroword;
-			regs[5]  <= `zeroword;
-			regs[6]  <= `zeroword;
-			regs[7]  <= `zeroword;
-			regs[8]  <= `zeroword;
-			regs[9]  <= `zeroword;
-			regs[10] <= `zeroword;
-			regs[11] <= `zeroword;
-			regs[12] <= `zeroword;
-			regs[13] <= `zeroword;
-			regs[14] <= `zeroword;
-			regs[15] <= `zeroword;
-			regs[16] <= `zeroword;
-			regs[17] <= `zeroword;
-			regs[18] <= `zeroword;
-			regs[19] <= `zeroword;
-			regs[20] <= `zeroword;
-			regs[21] <= `zeroword;
-			regs[22] <= `zeroword;
-			regs[23] <= `zeroword;
-			regs[24] <= `zeroword;
-			regs[25] <= `zeroword;
-			regs[26] <= `zeroword;
-			regs[27] <= `zeroword;
-			regs[28] <= `zeroword;
-			regs[29] <= `zeroword;
-			regs[30] <= `zeroword;
-			regs[31] <= `zeroword;
-		end
-		else if(RegWrite) 
+	always@(negedge clk) begin
+		if(!rst_n) begin
+			regs[1]   <= `zeroword;
+			regs[`sp] <= 32'h7ffffff0;
+			regs[`gp] <= 32'h10000000;
+			regs[4]   <= `zeroword;
+			regs[5]   <= `zeroword;
+			regs[6]   <= `zeroword;
+			regs[7]   <= `zeroword;
+			regs[8]   <= `zeroword;
+			regs[9]   <= `zeroword;
+			regs[10]  <= `zeroword;
+			regs[11]  <= `zeroword;
+			regs[12]  <= `zeroword;
+			regs[13]  <= `zeroword;
+			regs[14]  <= `zeroword;
+			regs[15]  <= `zeroword;
+			regs[16]  <= `zeroword;
+			regs[17]  <= `zeroword;
+			regs[18]  <= `zeroword;
+			regs[19]  <= `zeroword;
+			regs[20]  <= `zeroword;
+			regs[21]  <= `zeroword;
+			regs[22]  <= `zeroword;
+			regs[23]  <= `zeroword;
+			regs[24]  <= `zeroword;
+			regs[25]  <= `zeroword;
+			regs[26]  <= `zeroword;
+			regs[27]  <= `zeroword;
+			regs[28]  <= `zeroword;
+			regs[29]  <= `zeroword;
+			regs[30]  <= `zeroword;
+			regs[31]  <= `zeroword;
+		end else if(RegWrite) 
 			regs[Rd] <= Wr_data;
 	end
 
